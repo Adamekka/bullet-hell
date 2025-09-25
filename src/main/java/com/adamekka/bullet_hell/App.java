3b9ebc36e5050c8d357de53a8bdfaec59977f455
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2025-09-25 15:27:05 +0200

    feat: window

--- src/main/java/com/adamekka/bullet_hell/App.java
+++ src/main/java/com/adamekka/bullet_hell/App.java
@@ -1,5 +1,29 @@
 package com.adamekka.bullet_hell;
 
-public class App {
-    public static void main(String[] args) {}
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+
+        Canvas canvas = new Canvas(
+            Config.Window.SIZE.getWidth(), Config.Window.SIZE.getHeight()
+        );
+
+        Scene scene = new Scene(
+            root, Config.Window.SIZE.getWidth(), Config.Window.SIZE.getHeight()
+        );
+
+        primaryStage.setTitle(Config.Window.TITLE);
+        primaryStage.resizableProperty().setValue(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> System.exit(0));
+    }
 }
---
+++ src/main/java/com/adamekka/bullet_hell/Config.java
@@ -0,0 +1,10 @@
+package com.adamekka.bullet_hell;
+
+import javafx.geometry.Dimension2D;
+
+public final class Config {
+    public final static class Window {
+        static final String TITLE = "Bullet Hell";
+        static final Dimension2D SIZE = new Dimension2D(800, 600);
+    }
+}
